021ab442f917e2d9927128e110c1684841a8f14b
Kai <Kai@MacBook Pro>
2020-10-14 12:54:03 -0600

    - Added a testing solution for testing

---
+++ TestingArea/TestsForTestingArea/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestingArea;
+
+namespace TestsForTestingArea
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Class1 t = new Class1();
+
+            Assert.IsTrue(t.match("0x123FB"));
+        }
+    }
+}
